bd73e6d0ac42517e0a1d24e643ccf94274f13f51
Bau <bauvannguyen123@gmail.com>
2017-12-05 02:46:46 +0000

    Initial commit

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+    private Rigidbody rb;
+    public float speed;
+    private int count;
+    public Text countText;
+    public Text scoreText;
+
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        //If our object colldies with an object with the tag "PickUp", do the following.
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            count = count + 1;
+            other.gameObject.SetActive(false);
+            SetCountText();
+        }
+    }
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >=8)
+        {
+            scoreText.text = "You have won!";
+        }
+    }
+
+}
+
